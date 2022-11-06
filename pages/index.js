a98cd09c7aabbe4734100765debe8e99d0d64724
itscarew <itscarew@gmail.com>
2022-11-06 17:59:04 +0100

    new changes

--- pages/index.js
+++ pages/index.js
@@ -4,34 +4,22 @@ import { Switch } from "@headlessui/react";
 import { GiNightSleep } from "react-icons/gi";
 import { IoIosSunny } from "react-icons/io";
 
-import { Disclosure } from "@headlessui/react";
-import { ChevronUpIcon } from "@heroicons/react/solid";
-
-import Social from "@components/Social";
-import Jobs from "@components/Jobs";
-
 const Index = () => {
   const [enabled, setEnabled] = useState(false);
   return (
     <Layout
       className={`${
-        enabled ? "bg-black text-gray-100  " : ""
+        !enabled ? "bg-black text-gray-100  " : ""
       } transition ease-in-out duration-200 `}
     >
       <div>
         <div className="relative w-full flex items-center justify-between">
           {" "}
-          <span className="text-5xl">
-            {" "}
-            Ẹ Ǹlẹ́ !{" "}
-            {/* <span role={"img"} aria-label="hi">
-              👋
-            </span>{" "} */}{" "}
-          </span>
+          <span className="text-5xl"> Ẹ Ǹlẹ́ ! </span>
           <div className="flex items-center">
             <div className="mr-2">
               {" "}
-              <IoIosSunny size="2rem" />{" "}
+              <GiNightSleep size="2rem" />
             </div>
             <Switch
               checked={enabled}
@@ -48,7 +36,7 @@ const Index = () => {
               />
             </Switch>
             <div className="ml-2">
-              <GiNightSleep size="2rem" />
+              <IoIosSunny size="2rem" />{" "}
             </div>
           </div>
         </div>
@@ -65,74 +53,91 @@ const Index = () => {
           solution.
         </div>
 
-        <div className="flex lg:flex-row flex-col-reverse items-start">
-          <div className="lg:w-1/2 w-full lg:pt-16 pt-4">
-            <div
-              className={`w-full lg:p-1 p-2 mx-auto rounded-2xl ${
-                enabled ? "bg-gray-900" : ""
-              } `}
-            >
-              <Disclosure>
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-white   bg-sky-900 bg-opacity-75 rounded-lg hover:bg-sky-700 focus:outline-none focus-visible:ring focus-visible:ring-sky-700 focus-visible:ring-opacity-75">
-                      <span className="text-base">Skill</span>
-                      <ChevronUpIcon
-                        className={`${
-                          open ? "transform rotate-180" : ""
-                        } w-5 h-5 text-sky-500`}
-                      />
-                    </Disclosure.Button>
-                    <Disclosure.Panel
-                      className={`px-4 pt-4 pb-2 text-base font-black ${
-                        enabled ? "" : "text-gray-900"
-                      } `}
-                    >
-                      HTML, CSS, JavaScript, Typescript, React, Redux, SASS,
-                      Nodejs , Nextjs , Redis, Express, NestJS
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-              <Disclosure as="div" className="mt-2">
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-white   bg-sky-900 bg-opacity-75 rounded-lg hover:bg-sky-700 focus:outline-none focus-visible:ring focus-visible:ring-sky-700 focus-visible:ring-opacity-75">
-                      <span className="text-base">Education </span>
-                      <ChevronUpIcon
-                        className={`${
-                          open ? "transform rotate-180" : ""
-                        } w-5 h-5 text-sky-500`}
-                      />
-                    </Disclosure.Button>
-                    <Disclosure.Panel
-                      className={`px-4 pt-4 pb-2 text-base ${
-                        enabled ? "" : "text-gray-900"
-                      } `}
-                    >
-                      <ol className="list-disc text-base">
-                        <li>
-                          {" "}
-                          National Diploma , Computer Science, Yaba college of
-                          technology{" "}
-                        </li>
-                        <li> Honours Diploma, Software Engineering, NIIT. </li>
-                        <li>
-                          {" "}
-                          Bachelors Degree, Computer Science, ESGT University.{" "}
-                        </li>
-                      </ol>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            </div>
-            <Social enabled={enabled} />
-          </div>
+        <div className="mt-3">
+          I'm proficient in{" "}
+          <span className="font-black">
+            {" "}
+            HTML, CSS, JavaScript, Typescript{" "}
+          </span>{" "}
+          and modern frameworks like{" "}
+          <span className="font-black">
+            {" "}
+            React, Redux, SASS, Nodejs , Nextjs , Redis, Express, NestJS
+          </span>
+          and more{" "}
+        </div>
 
-          <div className="lg:w-1/2 w-full">
-            <Jobs enable={enabled} />
-          </div>
+        <div className="mt-3">
+          Innovative <span className="font-black"> software developer </span>{" "}
+          with 6 years experience in building pixel-perfect and customer-focused
+          websites, UIs and server-side applications, deploying robust and
+          secure APIs, meeting high standards web design, user experience, best
+          practices, and speed. I aim to make a difference through my creative
+          solution.
+        </div>
+
+        <div className="mt-3">
+          I reside in Lagos, Nigeria. I'm currently working as a frontend
+          developer in{" "}
+          <span className="font-black">
+            {" "}
+            Radeus Business Tech Limited, Lagos, Nigeria.{" "}
+          </span>{" "}
+          I've worked as a frontend developer at{" "}
+          <span className="font-black">
+            {" "}
+            Payday, Canada and Rwanda{" "}
+          </span> and{" "}
+          <span className="font-black"> Zijela ICT, Lagos, Nigeria </span>.
+        </div>
+
+        <div className="mt-3">
+          I have a{" "}
+          <span className="font-black">
+            {" "}
+            National Diploma in Computer Science{" "}
+          </span>{" "}
+          from Yaba college of technology, a{" "}
+          <span className="font-black">
+            {" "}
+            Honours Diploma in Software Engineering
+          </span>{" "}
+          from NIIT and a{" "}
+          <span className="font-black">
+            {" "}
+            Bachelors degree in Computer Science{" "}
+          </span>{" "}
+          from ESGT University.
+        </div>
+
+        <div className="mt-3">
+          This is my{" "}
+          <a
+            href="https://github.com/itscarew?tab=repositories"
+            target="/_blank"
+            className="font-black"
+          >
+            github{" "}
+          </a>{" "}
+          repository . My{" "}
+          <a
+            href="https://www.linkedin.com/in/olaonipekun-carew/"
+            target="/_blank"
+            className="font-black"
+          >
+            linkedin
+          </a>{" "}
+          profile. And Here is my{" "}
+          <a className="font-black" download href="/resume.pdf">
+            {" "}
+            resume{" "}
+          </a>
+          .{" "}
+        </div>
+
+        <div className="mt-3 mb-2">
+          Outside of writing code, I love watching movies, especially sci-fi
+          series . I also love to watch/play basketball and american football .
         </div>
 
         <footer> &copy; {new Date().getFullYear()} Me </footer>
